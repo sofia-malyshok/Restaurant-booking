4c8c6f7b16bcf8a7912c77323dac0b372341148c
Nazar Pechevystyi <pechevystyi@vialutions.com>
2022-05-21 16:10:36 +0200

    frontend initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      hello?<Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
