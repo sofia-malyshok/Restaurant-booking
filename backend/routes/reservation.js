1b7044fd8ede8d0a760a8556c492afb4da52f6f9
sofia-malyshok <soniamalish17@gmail.com>
2022-05-29 21:47:39 +0200

    add: routs for get reservation /delete resevations

--- backend/routes/reservation.js
+++ backend/routes/reservation.js
@@ -1,6 +1,8 @@
-const { createReservation } = require("../controllers/reservation");
+const { createReservation, getReservations, deleteResevation } = require("../controllers/reservation");
 const authenticate = require("../middleware/authenticate");
 
 module.exports = (router) => {
   router.route("/reservation").post(authenticate, createReservation);
+  router.route("/user/:id/reservations").get(authenticate, getReservations);
+  router.route("/reservation/:id").delete(authenticate, deleteResevation);
 };
